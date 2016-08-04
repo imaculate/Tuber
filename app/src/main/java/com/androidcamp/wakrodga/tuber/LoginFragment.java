3956fc78a829b4f22227e84dd001acfefa6c095d
Demo User <demouser@MacBook-Air.local>
2016-08-04 09:24:41 -0700

    Try

---
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/LoginFragment.java
@@ -0,0 +1,69 @@
+package com.androidcamp.wakrodga.tuber;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+    private LoginButton loginButton;
+
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        System.out.println("fragment on create view");
+       // FacebookSdk.sdkInitialize(getApplicationContext());
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(LoginActivity.callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
@@ -1,7 +1,9 @@
 package com.androidcamp.wakrodga.tuber;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
