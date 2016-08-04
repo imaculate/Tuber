02559e6f52536ae5f29582a682bbf838fd46a2b6
Demo User <demouser@MacBook-Air.local>
2016-08-04 12:08:02 -0700

    nothing

--- app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        
+
     }
 
     @Override
