d1b4e17ef17477596e8002799e902e7f57f67d99
Demo User <demouser@MacBook-Air.local>
2016-08-04 08:28:35 -0700

    alex

--- app/src/main/java/com/androidcamp/wakrodga/tuber/SearchActivity.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/SearchActivity.java
@@ -2,15 +2,49 @@ package com.androidcamp.wakrodga.tuber;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by demouser on 8/4/16.
  */
 public class SearchActivity extends AppCompatActivity {
 
+    private Spinner spinner1;
+    private Button btnSubmit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MyAndroidAppActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
