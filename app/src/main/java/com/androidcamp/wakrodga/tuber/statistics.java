12955b97b70f050c84b5ec960b95b250348bdd02
Demo User <demouser@MacBook-Air.local>
2016-08-04 20:14:38 +0100

    XXD

--- app/src/main/java/com/androidcamp/wakrodga/tuber/statistics.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/statistics.java
@@ -2,12 +2,39 @@ package com.androidcamp.wakrodga.tuber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class statistics extends AppCompatActivity {
+public class Statistics extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        String msg = "";
+        switch (menuItem.getItemId()) {
+            case R.id.edit:
+                Toast.makeText(this, "EDITED", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.log_out:
+                Toast.makeText(this, "LOGED_OUT", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
