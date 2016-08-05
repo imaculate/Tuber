f468c51a52439d5102ef9a91175cebf77f348d6d
Demo User <demouser@MacBook-Air.local>
2016-08-05 05:20:28 -0700

    position arguments to fragments

--- app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/MainActivity.java
@@ -170,7 +170,7 @@ public class MainActivity extends AppCompatActivity implements AllTutorsFragment
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return  new AllTutorsFragment();
+            return  AllTutorsFragment.newInstance(position);
         }
 
         @Override
