f558347fd84378e11469f9d7991fb753d78c39f2
Demo User <demouser@MacBook-Air.local>
2016-08-05 00:02:28 -0700

    Favorite button functionality

--- app/src/main/java/com/androidcamp/wakrodga/tuber/AllTutorsFragment.java
+++ app/src/main/java/com/androidcamp/wakrodga/tuber/AllTutorsFragment.java
@@ -4,7 +4,9 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Outline;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -236,13 +238,19 @@ public class AllTutorsFragment extends Fragment  {
                 holder.ratingBar = (RatingBar) view.findViewById(R.id.radingBar);
                 holder.city = (TextView) view.findViewById(R.id.city);
                 holder.image = (ImageView) view.findViewById(R.id.tutor_image);
-                holder.image.setOnClickListener(new View.OnClickListener() {
+                holder.like = (ImageView)view.findViewById(R.id.fav);
+                holder.like.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        holder.image.setImageResource(R.drawable.ic_favorite_black_36dp);
-                        holder.image.setColorFilter(Color.argb(255, 0, 0, 255));
+                        if(((ImageView)view).getContentDescription()=="border") {
+                            holder.like.setContentDescription("fill");
+                            holder.like.setImageResource(R.drawable.ic_favorite_black_36dp);
 
-
+                        }else{
+                            holder.like.setContentDescription("border");
+                            holder.like.setImageResource(R.drawable.ic_favorite_border_black_36dp);
+                        }
+                        holder.like.setColorFilter(Color.argb(255, 255, 0, 0));
                     }
                 });
 
@@ -278,6 +286,7 @@ public class AllTutorsFragment extends Fragment  {
             public RatingBar ratingBar;
             public TextView city;
             public ImageView image;
+            public ImageView like;
             public TextView subjects;
 
         }
